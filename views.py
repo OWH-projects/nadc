c4e110ce79144a09aad244ccb1291903b7c2e762
Ben Vankat <bvankat@gmail.com>
2015-12-11 22:34:02 +0000

    uuuuuuuuuunnnnnnnhhhhh yeah

--- views.py
+++ views.py
@@ -70,108 +70,51 @@ def Search(request):
     return render_to_response('nadc/search.html', dictionaries)    
     
 def EntityPage(request, id):
- 
-    #Test to see if we're dealing with a candidate record or not
-    if Candidate.objects.filter(cand_id=id).count() > 0:
-        candidatepage = True
-    else:
-        candidatepage = False
-    
-    #Candidate view
-    if candidatepage == True:
-        candidates = Candidate.objects.filter(cand_id=id)
-        committees = Candidate.objects.filter(cand_id=id).values('committee', 'committee__standard_name').distinct()
+    alldonations = Donation.objects.filter(Q(recipient__nadcid=id)|Q(donor__canonical=id)).order_by('donation_date')    
 
-        dictionaries = {'candidates':candidates, 'committees':committees,}
-        return render_to_response('nadc/candidate.html', dictionaries)
-    
-    #Any single Entity view
-    if candidatepage == False:
-        try:
-            comm_candidates = Candidate.objects.filter(committee=id)
-        except:
-            comm_candidates = []       
+    try:
+        comm_candidates = Candidate.objects.filter(committee=id)
+    except:
+        comm_candidates = []       
 
-        alldonations = Donation.objects.filter(Q(recipient__nadcid=id)|Q(donor__canonical=id)).order_by('donation_date')
-        try:
-            first = alldonations[0].donation_date
-            mostrecent = alldonations.reverse()[0].donation_date
-        except:
-            first = []
-            mostrecent = []
-        truncate_date = connection.ops.date_trunc_sql('month', 'donation_date')
-
-        try:
-            name = Entity.objects.filter(canonical=id)[0]
-        except:
-            name = Candidate.objects.filter(cand_id=id)[0]
+    try:
+        first = alldonations[0].donation_date
+        mostrecent = alldonations.reverse()[0].donation_date
+    except:
+        first = []
+        mostrecent = []
         
-        # Get any/all records of donations given by entity
-        try:
-            gives = Donation.objects.filter(donor__canonical=id).order_by("-donation_date")
-            allgives = gives.values('donation_date').annotate(total=Sum('cash') + Sum('inkind')).order_by('donation_date')
-            groupedgives = gives.values('recipient__name', 'recipient__nadcid').annotate(total=Sum('cash') + Sum('inkind')).order_by('-total')
-            topgives = groupedgives[:5]
-            topgivestotal = 0
-            for obj in topgives:
-                topgivestotal = topgivestotal + obj['total']
-            othergivestotal = allgives.aggregate(Sum('total'))['total__sum'] - topgivestotal
-            givesbymonth = allgives.extra({'month':truncate_date})
-            totalcashdonated = gives.aggregate(Sum('cash'))
-            totalinkinddonated = gives.aggregate(Sum('inkind'))
-        except:
-            gives = []
-            groupedgives= []
-            allgives = []
-            topgives = []
-            othergivestotal = []
-            givesbymonth = []
-            totalcashdonated = []
-            totalinkinddonated = []
+    truncate_date = connection.ops.date_trunc_sql('month', 'donation_date')
 
-        # Get any/all records of donations received by entity    
-        try:
-            gets = Donation.objects.filter(recipient__nadcid=id).order_by("-donation_date")
-            allgets = gets.values('donation_date').annotate(total=Sum('cash') + Sum('inkind')).order_by('donation_date')
-            groupedgets = gets.values('donor__standard_name', 'donor__canonical').annotate(total=Sum('cash') + Sum('inkind')).order_by('-total')
-            topgets =  groupedgets[:5]
-            topgetstotal = 0
-            for obj in topgets:
-                topgetstotal = topgetstotal + obj['total']
-            othergetstotal = allgets.aggregate(Sum('total'))['total__sum'] - topgetstotal
-            getsbymonth = allgets.extra({'month':truncate_date})
-            totalcashreceived = gets.aggregate(Sum('cash'))
-            totalinkindreceived = gets.aggregate(Sum('inkind'))
-        except:
-            gets = []
-            topgets = []
-            groupedgets = []
-            topgetstotal = []
-            totalcashreceived = []
-            totalinkindreceived = []        
-            othergetstotal = []
-            getsbymonth = []
-            
-        # Expenditures
-        try:
-            normal_expenditures = Expenditure.objects.filter(committee=id).filter(raw_target="").order_by('-exp_date')
-            normal_spent = normal_expenditures.aggregate(Sum("amount"))
-            ind_expenditures = Expenditure.objects.filter(committee=id).exclude(raw_target="").order_by('-exp_date')
-            ind_spent = ind_expenditures.aggregate(Sum("amount"))
-        except:
-            normal_expenditures = []
-            normal_spent = []
-            ind_expenditures = []
-            ind_spent = []
+    name = Entity.objects.filter(canonical=id)[0]
+    
+    # Get any/all records of donations given by entity
+    try:
+        gives = Donation.objects.filter(donor__canonical=id).order_by("-donation_date")
+    except:
+        gives = []
 
-        # Loans
-        try:
-            loans = Loan.objects.filter(committee=id).order_by('-loan_date')
-            totalborrowed = loans.aggregate(Sum("loan_amount"))
-        except:
-            loans = []
-            totalborrowed - []
+    # Get any/all records of donations received by entity    
+    try:
+        gets = Donation.objects.filter(recipient__nadcid=id).order_by("-donation_date")
+    except:
+        gets = []
+        
+    # Expenditures
+    try:
+        normal_expenditures = Expenditure.objects.filter(committee=id).filter(raw_target="").order_by('-exp_date')
+        ind_expenditures = Expenditure.objects.filter(committee=id).exclude(raw_target="").order_by('-exp_date')
+    except:
+        normal_expenditures = []
+        ind_expenditures = []
 
-        dictionaries = {'id': id, 'groupedgets': groupedgets, 'topgetstotal': topgetstotal, 'othergetstotal': othergetstotal, 'othergivestotal':othergivestotal, 'getsbymonth':getsbymonth, 'givesbymonth':givesbymonth, 'groupedgives': groupedgives, 'allgives': allgives, 'comm_candidates': comm_candidates, 'topgets': topgets, 'totalcashreceived': totalcashreceived, 'totalinkindreceived': totalinkindreceived, 'topgives': topgives, 'totalcashdonated': totalcashdonated, 'totalinkinddonated': totalinkinddonated,'gives': gives, 'gets': gets, 'normal_expenditures': normal_expenditures, 'normal_spent': normal_spent, 'ind_expenditures': ind_expenditures, 'ind_spent': ind_spent, 'loans': loans, 'totalborrowed': totalborrowed, 'name':name, 'first':first,'mostrecent':mostrecent, }
-        return render_to_response('nadc/entity.html', dictionaries)
+    # Loans
+    try:
+        loans = Loan.objects.filter(committee=id).order_by('-loan_date')
+    except:
+        loans = []
+
+    dictionaries = {'id': id, 'comm_candidates': comm_candidates, 'gives': gives, 'gets': gets, 'normal_expenditures': normal_expenditures, 'ind_expenditures': ind_expenditures, 'loans': loans, 'name':name, 'first':first, 'mostrecent':mostrecent, }
+    
+    return render_to_response('nadc/entity.html', dictionaries)
 
