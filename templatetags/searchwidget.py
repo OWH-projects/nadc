fdf618eb2fbfa8490a5576675b0e9fda98e140ea
Ben Vankat <bvankat@gmail.com>
2015-12-31 21:07:44 +0000

    womped a bunch of loan shiz on entity page

--- templatetags/searchwidget.py
+++ templatetags/searchwidget.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 
 def returnsearch():
-    str = '<h3 class="bold">Search</h3><form id="entity" action="/campaign-finance/search" method="GET"><div class="input-group"><input type="text" class="form-control input-sm" name="q" value="" placeholder="Campaign, donor or PAC"><span class="input-group-btn"><button class="btn btn-primary btn-sm" type="submit">Search</button></span></div></form>'    
+    str = '<h3 class="bold">Search</h3><form id="entity" action="/campaign-finance/search" method="GET"><div class="input-group"><input type="text" class="form-control input-sm" name="q" value="" placeholder="Campaign, donor, PAC or ZIP code"><span class="input-group-btn"><button class="btn btn-primary btn-sm" type="submit">Search</button></span></div></form>'    
     return str
 
 register.simple_tag(returnsearch)
